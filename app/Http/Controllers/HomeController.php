c152170fb81f879b0144c7046e7d2642977d9fa5
mayank07feb <makedy07feb@gmail.com>
2024-08-23 10:45:55 +0530

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('frontend.index');
+    }
+
+    public function about()
+    {
+        return view('frontend.about');
+    }
+
+    public function blog()
+    {
+        return view('frontend.blog');
+    }
+
+    public function contact()
+    {
+        return view('frontend.contact');
+    }
+
+    public function course()
+    {
+        return view('frontend.course');
+    }
+
+    public function single()
+    {
+        return view('frontend.single');
+    }
+
+    public function teacher()
+    {
+        return view('frontend.teacher');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/blog', [HomeController::class, 'blog'])->name('blog');
+Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+Route::get('/course', [HomeController::class, 'course'])->name('course');
+Route::get('/single', [HomeController::class, 'single'])->name('single');
+Route::get('/teacher', [HomeController::class, 'teacher'])->name('teacher');
+
